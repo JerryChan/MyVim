d85d72f0704d60459a148543fbe83a608bd505bf
Cong Ling <adonis0147@yahoo.com.cn>
2012-09-02 20:10:49 +0800

    v0.1.2

--- install.sh
+++ install.sh
@@ -11,10 +11,6 @@ sudo apt-get install clang
 
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-mkdir -p ~/.vim/colors
-wget -O ~/.vim/colors/wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
-vim ~/.vim/colors/wombat256mod.vim -c "%s/234/None/g" -c "%s/232/None/g" -c "w" -c "q"
-
 cp ./.vimrc ~/.vimrc
 
 vim -c "BundleInstall" -c "qa"
